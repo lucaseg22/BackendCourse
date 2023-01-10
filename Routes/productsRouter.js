dc06ebd9d95d003e07af7a7a614cf033cf3d5eca
Gomez Lucas <lucasezequielgomez22@gmail.com>
2023-01-09 21:27:45 -0300

    Edit de reordenamiento

--- Routes/productsRouter.js
+++ Routes/productsRouter.js
@@ -1,24 +1,46 @@
 const express = require('express')
 const productsRouter = express.Router()
-let products = require('../database/products.json');
 const fs = require('fs');
+
+let _products = require('../database/products.json');
+let products = []
 
 productsRouter.get('/', (req, res) => {
     const limit = req.query.limit
 
     if(limit && !isNaN(Number(limit)))
     {
-        let response = products.slice(0,limit)
+        let response = _products.slice(0,limit)
         res.send(response);
     }
-    res.send(products)
+    res.send(_products)
 });
 productsRouter.get('/:pid', (req, res) => {
     const pid = req.params.pid  
-    product = products.find((e) => e.id == pid);
+    product = _products.find((e) => e.id == pid);
     console.log(product)
     res.send(product)
 });
+
+productsRouter.post('/', (req, res) => {
+
+    let length = Object.keys(_products).length
+    let lengthUpdate = length + 1
+    let product = req.body
+
+    product = { ...product, id: lengthUpdate }
+    if (product) {
+        console.log(req.body)
+        _products.push(product) 
+        fs.writeFileSync('./database/products.json', JSON.stringify(_products));
+        return res.send('Ok');
+        
+    }  else {
+        console.log('No cargado')
+    }
+
+    res.status(400).send("Bad request");
+})
 
 productsRouter.put('/:pid', (req, res) => {
     //Actualizar producto
@@ -27,7 +49,7 @@ productsRouter.put('/:pid', (req, res) => {
     let updateId = req.params -1
     console.log(+req.params.pid - 1, 'put - productsid')
 
-    products[+req.params.pid - 1] = {
+    _products[+req.params.pid - 1] = {
         title: req.body.title,
         description: req.body.description,
         price: req.body.price,
@@ -37,18 +59,18 @@ productsRouter.put('/:pid', (req, res) => {
         path: req.body.path,
         id: +req.params.pid
     }
-    fs.writeFileSync('./database/products.json', JSON.stringify(products));
+    fs.writeFileSync('./database/products.json', JSON.stringify(_products));
     console.log(update, 'put - update')
-    console.log(products, 'put - postupdate')
+    console.log(_products, 'put - postupdate')
     res.send('Producto actualizado con exito!')
 });
 
 productsRouter.delete('/:pid', (req, res) => {
 
-    products = products.filter((e) => e.id !== +req.params.pid)
-    console.log('del', products)
+    _products = _products.filter((e) => e.id !== +req.params.pid)
+    console.log('del', _products)
     
-    fs.writeFileSync('./database/products.json', JSON.stringify(products))
+    fs.writeFileSync('./database/products.json', JSON.stringify(_products))
 
     res.send('Producto eliminado!');
 
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const _products = require('./database/products.json')
 const fs = require('fs');
 const { cartsRouter } = require('./Routes/cartsRouter');
 
-let products = []
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
@@ -16,31 +16,6 @@ app.use(express.urlencoded({ extended: true}));
 
 app.get('/', function (req, res) {
     res.send('Hello World')
-})
-app.post('/', function (req, res) {
-
-    let length = Object.keys(_products).length
-    let lengthUpdate = length + 1
-    let product = req.body
-    console.log(_products, ' Aca')
-    product = { ...product, id: lengthUpdate }
-    console.log('ok pre if')
-
-    if (product) {
-        console.log(req.body)
-        _products.push(product) 
-        console.log('Llego al push')
-        
-        fs.writeFileSync('./database/products.json', JSON.stringify(_products));
-        products = []
-        return res.send('Ok');
-        
-    }  else {
-        console.log('No cargado')
-    }
-
-
-    res.status(400).send("Bad request");
 })
 
 app.use('/api/products', productsRouter);
